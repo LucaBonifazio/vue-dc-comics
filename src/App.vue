9cf32ed3e6fb6f2e9d689f265f032d871bae05be
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-24 14:34:43 +0100

    First commit with first step

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HeaderPart />
+    <MainPart />
+    <FooterPart />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HeaderPart from '@/components/HeaderPart.vue';
+import MainPart from '@/components/MainPart.vue';
+import FooterPart from '@/components/FooterPart.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld,
+    HeaderPart,
+    MainPart,
+    FooterPart,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
