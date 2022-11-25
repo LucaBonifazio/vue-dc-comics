1cc92bd5cd5a9e1b45c181eef123a717d82dab84
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-25 16:33:49 +0100

    Ending

---
+++ src/components/MainCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <div class="contents">
+      <img
+        :src="card.thumb"
+        :alt="card.series"
+      >
+      <h2>{{ card.series}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainCard',
+  props: {
+    card: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .card {
+    text-align: center;
+    padding: .1rem;
+    .contents {
+      padding: 1em;
+      height: 100%;
+      h2 {
+        color: white;
+        font-size: 1.2rem;
+      }
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+  }
+</style>
--- src/components/MainPart.vue
+++ src/components/MainPart.vue
@@ -1,29 +1,117 @@
 <template>
   <div class="bg_container">
     <section class="slider_image"></section>
-    <section class="cards_box">
-      <div class="card_box"></div>
+    <section class="cards_box container">
+      <main-card
+      v-for="objCard in arrCards"
+      :key="objCard.series"
+      class="card"
+      :card="objCard"
+      />
+      <button class="btn btn_current">CURRENT SERIES</button>
+      <button class="btn btn_load">LOAD MORE</button>
     </section>
   </div>
 </template>
 
 <script>
-// import MainCard from '@/components/MainCard.vue';
+import MainCard from '@/components/MainCard.vue';
 
 export default {
   name: 'MainPart',
+  components: {
+    MainCard,
+  },
+  data() {
+    return {
+      arrCards: [
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX',
+          price: '$19.99',
+          series: 'Action Comics',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01',
+          price: '$3.99',
+          series: 'American Vampire 1976',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb',
+          price: '$16.99',
+          series: 'Aquaman',
+          type: 'graphic novel',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76',
+          price: '$2.99',
+          series: 'Batgirl',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ',
+          price: '$3.99',
+          series: 'Batman',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ',
+          price: '$2.99',
+          series: 'Batman Beyond',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8',
+          price: '$3.99',
+          series: 'Batman/Superman',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk',
+          price: '$4.99',
+          series: 'Batman/Superman Annual',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8',
+          price: '$5.99',
+          series: 'Batman: The Joker War Zone',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY',
+          price: '$6.99',
+          series: 'Batman: Three Jokers',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX',
+          price: '$4.99',
+          series: 'Batman: White Knight Presents: Harley Quinn',
+          type: 'comic book',
+        },
+        {
+          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq',
+          price: '$16.99',
+          series: 'Catwoman',
+          type: 'graphic novel',
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .bg_container {
-  background-color: black;
+  background-color: rgb(29, 29, 29);
 }
 .slider_image {
   display: flex;
   justify-content: center;
   text-align: center;
-  height: 300px;
+  height: 500px;
   background-image: url('@/components/img/jumbotron.jpg');
   object-position: center;
   background-size: cover;
@@ -32,7 +120,37 @@ export default {
 .cards_box {
   display: flex;
   justify-content: center;
-  text-align: center;
-  height: 677px;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 2em 0;
+  position: relative;
+}
+.card {
+  flex: 0 0 calc(100% / 6);
+}
+
+.btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 150px;
+  height: 40px;
+  font-weight: bold;
+  color: white;
+  background-color: #0282f9;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.2);
+    background-color: #5db1ff;
+    color: black;
+  }
+}
+.btn_current {
+  position: absolute;
+  top: -20px;
+  left: 40px;
+  width: 200px;
+  height: 50px;
+  font-size: 1.2em;
 }
 </style>
